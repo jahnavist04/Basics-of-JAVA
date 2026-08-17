ccf6442717329eb9ad9d0aa2f86c6652d506ef15
JAHNAVI S T <jahnavist5@gmail.com>
2026-08-17 19:23:15 +0530

    Create Variables.java with basic data types example
    Add a Variables class demonstrating basic data types.

---
+++ 01-Basics/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 20;
+        double cgpa = 7.15;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Jahnavi";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
